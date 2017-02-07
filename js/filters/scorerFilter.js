619c45b4d437fbb8e8b36b2c0bb2a5a4cec60ce2
Lisa Coppinger <lc14g10@gmail.com>
2017-02-07 22:29:38 +0000

    implement scorer filter

--- js/filters/scorerFilter.js
+++ js/filters/scorerFilter.js
@@ -7,33 +7,23 @@ app.filter('scorerFilter', function($filter){
 
     angular.forEach(items, function(item) {
       angular.forEach(item, function(value, key) {
-        if (key == 'scorer1' && condition == value) {
-          filtered.push(item);
-        }
-      });
-    });
-
-/*
-    angular.forEach(items, function(item) {
-      //console.log(item.scorers);
-      angular.forEach(item, function(value, key) {
         if (key == 'scorer1' && value == condition) {
           filtered.push(item);
         }
       });
-      angular.forEach(item.scorers, function(value, key) {
-        angular.forEach(value, function(name, key) {
-          if (key == 'scorer' && name !== undefined) {
-            console.log(name);
-            if (name == condition) {
-              filtered.push(item);
-            }
-          }
-        });
-      });
     });
-    */
 
+    angular.forEach(items, function(item) {
+      if (item.scorers !== undefined) {
+        for(var i = 0; i < item.scorers.length; i++) {
+           if (item.scorers[i].scorer == condition) {
+             if (filtered.indexOf(item) == -1) {
+                  filtered.push(item);
+              }
+           }
+        }
+      }
+    });
     return filtered;
   };
 });
