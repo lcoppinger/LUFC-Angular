06d21fb484dd30198edf60ddc695ab003bd20b17
Lisa Coppinger <lisa.c.coppinger@uk.pwc.com>
2017-02-10 11:31:09 +0000

    remote json fix

--- js/services/filter.js
+++ js/services/filter.js
@@ -1,7 +1,7 @@
 app.factory('filter', ['$http', function($http){
 var selectOptions = {
   get: function(name) {
-    var promise = $http.get("/json/"+name+".json").then(function (response) {
+    var promise = $http.get("json/"+name+".json").then(function (response) {
        return response.data;
      }).then(function(err){
        return err;
